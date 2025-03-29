e53f32ffde172480c567ce21d3234a6d53fdf89e
VaishnaviPatole02 <patolevaishnavi0209@gmail.com>
2025-03-29 22:45:11 +0530

    Removed previous files

--- routes/budgetRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getBudgets, saveBudget } from "../controllers/budgetController.js";
-
-const router = express.Router();
-
-router.get("/get", getBudgets); // ✅ Fetch budgets
-router.post("/set", saveBudget); // ✅ Save budget
-
-export default router;
